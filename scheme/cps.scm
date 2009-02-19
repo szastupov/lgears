f7c5f4814b00bd3f3a72ed5957913b914ae81fc7
Stepan Zastupov <redchrom@gmail.com>
2009-02-19 16:06:11 +0300

    Added quotes support for cps
    Also all scheme sources retabed

--- scheme/cps.scm
+++ scheme/cps.scm
@@ -1,99 +1,119 @@
-(import (rnrs))
+(import (rnrs)
+        (quotes))
 
 (define orig '((define (f-aux n a)
-				 (if (= n 0)
-				   a
-				   (f-aux (- n 1) (* n a))))
+                 (if (= n 0)
+                   a
+                   (f-aux (- n 1) (* n a))))
 
-			   (define (factorial n)
-				 (if (= n 0)
-				   1
-				   (* n (factorial (- n 1)))))
+               (define (factorial n)
+                 (if (= n 0)
+                   1
+                   (* n (factorial (- n 1)))))
 
-			   (define (foo n)
-				 (bar (lambda (x) (+ x n))))
+               (define (foo n)
+                 (bar (lambda (x) (+ x n))))
 
-			   (define bar (lambda (x y) (+ x y (begin (display 12) (* 1 2 3)))))
+               (define bar (lambda (x y) (+ x y (begin (display 12) (* 1 2 3)))))
 
-			   (define (pyth x y)
-				 (sqrt (+ (* x x) (* y y))))))
+               (define (bla)
+                 (display '(a b c d)))
+
+               (define (pyth x y)
+                 (sqrt (+ (* x x) (* y y))))))
 
 (define last-name 0)
 
 (define (gen-name)
-  (let ((res (string-append "val" (number->string last-name 16))))
-	(set! last-name (+ last-name 1))
-	(string->symbol res)))
+  (let ((res (string-append "var" (number->string last-name 16))))
+    (set! last-name (+ last-name 1))
+    (string->symbol res)))
 
 (define (echo n)
   (display n)
   (newline))
 
+(define (self-eval? node)
+  (or (not (pair? node))
+      (and (eq? (car node) 'quote)
+           (not (pair? (cadr node))))))
+
+(define (convert-quote res node func name)
+  (if (pair? node)
+    (convert res (func node) name)
+    node))
+
 (define (convert res node name)
   (if (pair? node)
-	(case (car node)
-	  ((lambda)
-	   `(lambda (,@(cadr node) ,name)
-		  ,(convert-body res (cddr node) name)))
+    (case (car node)
+      ((lambda)
+       `(lambda (,@(cadr node) ,name)
+          ,(convert-body res (cddr node) name)))
 
-	  ((if)
-	   (let* ((args (cdr node))
-			  (predname (gen-name)))
-		 (convert `(if ,predname
-					 ,(convert '() (cadr args) name)
-					 ,(convert '() (caddr args) name))
-				  (car args) predname)))
+      ((if)
+       (let* ((args (cdr node))
+              (predname (gen-name)))
+         (convert `(if ,predname
+                     ,(convert '() (cadr args) name)
+                     ,(convert '() (caddr args) name))
+                  (car args) predname)))
 
-	  ((begin)
-	   (let* ((seq (reverse (cdr node)))
-			  (frst (car seq)))
-		 (fold-left (lambda (prev x)
-					  (convert prev x (if (eq? x frst)
-										name
-										(gen-name))))
-					res seq)))
+      ((begin)
+       (let* ((seq (reverse (cdr node)))
+              (frst (car seq)))
+         (fold-left (lambda (prev x)
+                      (convert prev x (if (eq? x frst)
+                                        name
+                                        (gen-name))))
+                    res seq)))
 
-	  (else
-		(let* ((args (reverse (cdr node)))
-			   (largs (map (lambda (x)
-							 (if (pair? x)
-							   (gen-name)
-							   x))
-						   args))
-			   (tail (if (null? res)
-					   name
-					   `(lambda (,name) ,res)))
-			   (expr `(,(car node) ,@(reverse largs) ,tail)))
-		  (fold-left (lambda (prev x n)
-					   (if (pair? x)
-						 (convert prev x n)
-						 prev))
-					 expr args largs))))
-	(if (null? res)
-	  (list name node)
-	  `(,node (lambda (,name) ,res)))))
+      ((quote)
+       (convert-quote res (cadr node) trquote name))
+
+      ((quasiquote)
+       (convert-quote res (cadr node) trquasiquote name))
+
+      (else
+        (let* ((args (reverse (cdr node)))
+               (largs (map (lambda (x)
+                             (if (self-eval? x)
+                               x
+                               (gen-name)))
+                           args))
+               (control (if (null? res)
+                          name
+                          `(lambda (,name) ,res)))
+               (expr `(,(car node) ,control ,@(reverse largs))))
+          (fold-left (lambda (prev x n)
+                       (if (self-eval? x)
+                         prev
+                         (convert prev x n)))
+                     expr args largs))))
+    (if (null? res)
+      (list name node)
+      `((lambda (,name) ,res) ,node))))
 
 (define (convert-define def)
   (cond ((pair? (car def))
-		 (let ((name (gen-name)))
-		   `(define ,(caar def)
-			  (lambda (,@(cdar def) ,name)
-				,(convert-body '() (cdr def) name)))))
-		((pair? (cadr def))
-		 `(define ,(car def)
-			,(convert '() (cadr def) (gen-name))))
-		(else def)))
+         (let ((name (gen-name)))
+           `(define ,(caar def)
+              (lambda (,@(cdar def) ,name)
+                ,(convert-body '() (cdr def) name)))))
+        ((pair? (cadr def))
+         `(define ,(car def)
+            ,(convert '() (cadr def) (gen-name))))
+        (else def)))
 
 (define (convert-body res body name)
   (let* ((seq (reverse body))
-		 (frst (car seq)))
-	(fold-left (lambda (prev x)
-				 (if (eq? (car x) 'define)
-				   (cons (convert-define (cdr x)) prev)
-				   (convert prev x (if (eq? x frst)
-									 name
-									 (gen-name)))))
-			   res (reverse body))))
+         (frst (car seq)))
+    (fold-left (lambda (prev x)
+                 (if (eq? (car x) 'define)
+                   (cons (convert-define (cdr x)) prev)
+                   (convert prev x (if (eq? x frst)
+                                     name
+                                     (gen-name)))))
+               res (reverse body))))
 
 (pretty-print
   (convert-body '() orig (gen-name)))
--- scheme/quotes.scm
+++ scheme/quotes.scm
@@ -3,42 +3,42 @@
   (import (rnrs))
 
   (define (self-eval? x)
-	(or (number? x)
-		(string? x)
-		(char? x)
-		(boolean? x)))
+    (or (number? x)
+        (string? x)
+        (char? x)
+        (boolean? x)))
 
   ; Common transquote routine
   (define (trquote-common qv pfunc)
-	(cons 'list 
-		  (let loop ((cur qv))
-			(cond ((null? cur)
-				   '())
-				  ((pair? cur)
-				   (cons (let ((head (car cur)))
-						   (cond ((pair? head)
-								  (pfunc head))
-								 ((self-eval? head)
-								  head)
-								 (else `(quote ,(car cur)))))
-						 (loop (cdr cur))))
-				  (else cur)))))
+    (cons 'list 
+          (let loop ((cur qv))
+            (cond ((null? cur)
+                   '())
+                  ((pair? cur)
+                   (cons (let ((head (car cur)))
+                           (cond ((pair? head)
+                                  (pfunc head))
+                                 ((self-eval? head)
+                                  head)
+                                 (else `(quote ,(car cur)))))
+                         (loop (cdr cur))))
+                  (else cur)))))
 
   ; Translate quotation to functions
   (define (trquote qv)
-	(trquote-common qv trquote))
+    (trquote-common qv trquote))
 
   ; Translate quoasiquotation to functions
   ; FIXME: implement splicing
   (define (trquasiquote qv)
-	(trquote-common
-	  qv
-	  (lambda (head)
-		(case (car head)
-		  ((unquote)
-		   (cadr head))
-		  ((unquote-splicing)
-		   (cadr head))
-		  (else
-			(trquasiquote head))))))
+    (trquote-common
+      qv
+      (lambda (head)
+        (case (car head)
+          ((unquote)
+           (cadr head))
+          ((unquote-splicing)
+           (cadr head))
+          (else
+            (trquasiquote head))))))
   )

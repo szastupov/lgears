b9705d3488e1d2dc6c7663ded8fe9ea1bfd16876
Stepan Zastupov <redchrom@gmail.com>
2009-04-01 04:22:24 +0400

    Added some code to the new expander

--- scheme/sc/expand.scm
+++ scheme/sc/expand.scm
@@ -1,24 +1,159 @@
 (library (sc expand)
   (export expand)
-  (import (except (rnrs) identifier?)
+  (import (except (rnrs) identifier? datum->syntax)
           (rnrs eval)
           (format)
           (sc gen-name)
           (only (core) pretty-print))
 
+  (define-record-type syntax-object
+    (fields expr wrap))
+
+  (define-record-type mark)
+
+  (define-record-type subst
+    (fields sym mark* label))
+
+  (define-record-type label)
+
   (define-record-type binding
     (fields type value))
 
-  (define-record-type syntax-object
-    (fields expr wrap))
+  (define top-mark (make-mark))
+
+  (define (top-marked? wrap)
+    (and (not (null? wrap))
+         (or (eq? (car wrap) top-mark)
+             (top-marked? (cdr wrap)))))
+
+  (define (strip x)
+    (cond ((syntax-object? x)
+           (if (top-marked? (syntax-object-wrap x))
+               (syntax-object-expr x)
+               (strip (syntax-object-expr x))))
+          ((pair? x)
+           (let ((a (strip (car x)))
+                 (d (strip (cdr x))))
+             (if (and (eq? a (car x)) (eq? d (cdr x)))
+                 x
+                 (cons a d))))))
+
+  (define (syntax-error what msg)
+    (error (strip what) msg))
 
   (define (identifier? x)
     (and (syntax-object? x)
          (symbol? (syntax-object-expr x))))
 
+  (define (syntax-pair? x)
+    (pair? (syntax-object-expr x)))
+
+  (define (syntax-car x)
+    (extend-wrap
+     (syntax-object-wrap x)
+     (car (syntax-object-expr x))))
+
+  (define (syntax-cdr x)
+    (extend-wrap
+     (syntax-object-wrap x)
+     (cdr (syntax-object-expr x))))
+
+  (define (syntax-cadr x)
+    (syntax-car (syntax-cdr x)))
+
+  (define (syntax-cddr x)
+    (syntax-cdr (syntax-cdr x)))
+
+  (define (syntax-caddr x)
+    (syntax-car (syntax-cddr x)))
+
+  (define (syntax-null? x)
+    (null? (syntax-object-expr x)))
+
+  (define (syntax->list x)
+    (if (syntax-null? x)
+        '()
+        (cons (syntax-car x)
+              (syntax->list (syntax-cdr x)))))
+
+  (define (datum->syntax template-id x)
+    (make-syntax-object x (syntax-object-wrap template-id)))
+
   (define (self-evaluating? x)
     (not (or (pair? x)
              (syntax-object? x))))
+
+  (define (wrap-marks wrap)
+    (if (null? wrap)
+      '()
+      (let ((w0 (car wrap)))
+        (if (mark? w0)
+          (cons w0 (wrap-marks (cdr wrap)))
+          (wrap-marks (cdr wrap))))))
+
+  (define (same-marks? m1* m2*)
+    (if (null? m1*)
+        (null? m2*)
+        (and (not (null? m2*))
+             (eq? (car m1*) (car m2*))
+             (same-marks? (cdr m1*) (cdr m2*)))))
+
+  (define (extend-wrap wrap x)
+    (if (syntax-object? x)
+        (make-syntax-object
+         (syntax-object-expr x)
+         (join-wraps wrap (syntax-object-wrap x)))
+        (make-syntax-object x wrap)))
+
+  (define (join-wraps wrap1 wrap2)
+    (cond ((null? wrap1) wrap2)
+          ((null? wrap2) wrap1)
+          (else
+           (let loop ((w (car wrap1))
+                      (w* (cdr wrap1)))
+             (if (null? w*)
+                 (if (and (mark? w)
+                          (eq? (car wrap2) w))
+                     (cdr wrap2)
+                     (cons w wrap2))
+                 (cons w (loop (car w*) (cdr w*))))))))
+
+  (define (add-mark mark x)
+    (extend-wrap (list mark) x))
+
+  (define (add-subst id label x)
+    (extend-wrap
+     (list (make-subst
+            (syntax-object-expr id)
+            (wrap-marks (syntax-object-wrap id))
+            label))
+     x))
+
+  (define (extend-env label binding env)
+    (cons (cons label binding) env))
+
+  (define (id-binding id r)
+    (label-binding id (id-label id) r))
+
+  (define (id-label id)
+    (let ((sym (syntax-object-expr id))
+          (wrap (syntax-object-wrap id)))
+      (let search ((wrap wrap)
+                   (mark* (wrap-marks wrap)))
+        (if (null? wrap)
+            (syntax-error id "undefined identifier")
+            (let ((w0 (car wrap)))
+              (if (mark? w0)
+                  (search (cdr wrap) (cdr mark*))
+                  (if (and (eq? (subst-sym w0) sym)
+                           (same-marks? (subst-mark* w0) mark*))
+                      (subst-label w0)
+                      (search (cdr wrap) mark*))))))))
+
+  (define (label-binding id label r)
+    (let ((a (assq label r)))
+      (if a (cdr a)
+          (syntax-error id "displaced lexical"))))
 
   (define (macro x)
     (make-binding 'macro x))
@@ -26,42 +161,10 @@
   (define (lexical x)
     (make-binding 'lexical x))
 
-  (define-record-type bind-env
-    (fields parent tbl)
-    (protocol
-      (lambda (new)
-        (lambda (prev bindings)
-          (let ((ntbl (make-eq-hashtable)))
-            (for-each (lambda (x)
-                        (hashtable-set! ntbl (car x) (cdr x)))
-                      bindings)
-            (new prev ntbl))))))
-
-  (define (bind-env-install env name binding)
-    (hashtable-set! (bind-env-tbl env) name binding))
-
-  (define (install-expander env name expander)
-    (bind-env-install env name (make-binding 'macro expander)))
-
-  (define (install-lexixal env name)
-    (let ((nname (gen-name name)))
-      (bind-env-install env name (make-binding 'lexical nname))
-      nname))
-
   (define (define-syntax? x)
     (and (pair? x)
          (eq? (car x) 'define-syntax)))
 
-  (define (make-expander m)
-    (make-binding 'macro
-                  (lambda (x e) (e (m x) e))))
-
-  (define (compile-macro x)
-    (cons (cadr x)
-          (make-expander
-            (eval (caddr x) (environment '(rnrs)
-                                         '(sc gen-name)
-                                         '(format))))))
   (define (splice-begin src)
     (let loop ((cur src))
       (cond ((null? cur)
@@ -72,53 +175,77 @@
             (else
               (cons (car cur) (loop (cdr cur)))))))
 
-  (define (install-top-level env)
-    (install-expander env 'quote (lambda (x e) x))
-    (install-expander env 'lambda (lambda (x e)
-                                    (expand env (cadr x) (cddr x))))
-    (install-expander env 'define (lambda (x e)
-                                    (if (pair? (cadr x))
-                                      `(define ,(caadr x)
-                                         ,(e `(lambda ,(cdadr x)
-                                                ,@(cddr x)) e))
-                                      `(define ,(cadr x)
-                                         ,(e (caddr x) e))))))
+  (define (exp-macro p x)
+    (let ((m (make-mark)))
+      (add-mark m (p (add-mark m x)))))
 
-  (define (expand prev vars body)
-    (let-values (((expanders source) (partition define-syntax?
-                                                (splice-begin body))))
-      (let ((env (make-bind-env prev (map compile-macro expanders))))
+  (define (exp-core p x r mr)
+    (p x r mr))
 
-        (define (binding? x type)
-          (let loop ((cur-env env))
-            (cond ((null? cur-env)
-                   #f)
-                  ((hashtable-ref (bind-env-tbl cur-env) x #f)
-                   => (lambda (b)
-                        (if (eq? (binding-type b) type)
-                          (binding-value b) #f)))
-                  (else
-                    (loop (bind-env-parent cur-env))))))
+  (define (exp-exprs x* r mr)
+    (map (lambda (x)
+           (exp-dispatch x r mr))
+         x*))
 
-        (define (initial-expander x e)
-          (cond ((symbol? x)
-                 (cond ((binding? x 'lexical) => (lambda (x) x))
-                       (else x)))
-                ((not (pair? x)) x)
-                ((binding? (car x) 'macro) => (lambda (b)
-                                                 (b x e)))
-                (else (map (lambda (x) (e x e)) x))))
+  (define (exp-dispatch x r mr)
+    (cond ((identifier? x)
+           (let ((b (id-binding x r)))
+             (case (binding-type b)
+               ((macro) (exp-macro (binding-value b) x) r mr)
+               ((lexical)(binding-value b))
+               (else (syntax-error x "invalid syntax")))))
+          ((not (syntax-pair? x))
+           (strip x))
+          ((identifier? (syntax-car x))
+           (let ((b (id-binding (syntax-car x) r)))
+             (case (binding-type b)
+               ((macro) (exp-macro (binding-value b) x) r mr)
+               ((lexical)
+                `(,(binding-value b)
+                  ,@(exp-exprs (syntax->list (syntax-cdr x)) r mr)))
+               ((core) (exp-core (binding-value b) x r mr))
+               (else (syntax-error x "invalid syntax")))))
+          (else
+           `(,(exp-dispatch (syntax-car x) r mr)
+             ,@(exp-exprs (syntax->list (syntax-cdr x)) r mr)))))
 
-        (install-top-level env)
-        (let ((newbody (map (lambda (x)
-                              (initial-expander x initial-expander))
-                            source)))
-          (if (null? prev)
-            newbody
-            `(lambda ,(map (lambda (var)
-                             (install-lexixal env var))
-                           vars)
-               ,@newbody))))))
+  (define (exp-quote x r mr)
+    `(quote ,(strip (syntax-cadr x))))
 
-  ;(pretty-print (expand '() (read-source "test.scm")))
+  (define (make-lambda-env env labels new-vars)
+    (fold-left (lambda (prev l v)
+                 (extend-env l (make-binding 'lexical v) prev))
+               env labels new-vars))
+
+  (define (exp-lambda x r mr)
+    (let* ((vars (syntax->list (syntax-cadr x)))
+           (body (syntax-cddr x))
+           (new-vars (map (lambda (x)
+                            (gen-name (syntax-object-expr x)))
+                          vars))
+           (labels (map (lambda (x) (make-label)) vars))
+           (env (make-lambda-env r labels new-vars)))
+      `(lambda ,new-vars
+         ,@(exp-dispatch (fold-left (lambda (xpr id label)
+                                     (add-subst id label xpr))
+                                   body vars labels)
+                        env mr))))
+
+  (define (initial-wrap-end-env)
+    (define bindings
+      `((quote . ,(make-binding 'core exp-quote))
+        (lambda . ,(make-binding 'core exp-lambda))))
+    (let ((labels (map (lambda (x) (make-label)) bindings)))
+      (values
+       `(,@(map (lambda (sym label)
+                  (make-subst sym (list top-mark) label))
+                (map car bindings)
+                labels)
+         ,top-mark)
+       (map cons labels (map cdr bindings)))))
+
+  (define (expand x)
+    (let-values (((wrap env) (initial-wrap-end-env)))
+      (exp-dispatch (make-syntax-object x wrap) env env)))
+
   )
